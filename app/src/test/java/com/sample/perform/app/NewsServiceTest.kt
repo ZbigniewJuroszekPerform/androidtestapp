05db8ced94f310ca2dd514b5283cda2dba72af0d
ZbigniewJuroszekPerform <Zbigniew.Juroszek@performgroup.com>
2018-01-09 11:50:54 +0100

    three test cases added

--- app/src/test/java/com/sample/perform/app/NewsServiceTest.kt
+++ app/src/test/java/com/sample/perform/app/NewsServiceTest.kt
@@ -1,37 +1,39 @@
 package com.sample.perform.app
 
-import com.nhaarman.mockito_kotlin.any
-import com.nhaarman.mockito_kotlin.eq
-import com.nhaarman.mockito_kotlin.whenever
+
+import com.nhaarman.mockito_kotlin.*
 import com.sample.perform.app.data.api.news.NewsBackendApi
 import com.sample.perform.app.data.model.Response
 import com.sample.perform.app.data.service.news.NewsService
-import io.reactivex.Observable
 import io.reactivex.Single
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mock
-import com.nhaarman.mockito_kotlin.any
-import com.nhaarman.mockito_kotlin.eq
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.whenever
-import com.sample.perform.app.data.model.ArticlesItem
-import java.util.*
-
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.mockito.ArgumentMatchers
 
 
 class NewsServiceTest {
 
     private lateinit var systemUnderTest: NewsService
-
     private  val newsBackendApi : NewsBackendApi = mock()
 
         @Before
         fun setUp(){
-            whenever(newsBackendApi.getNews(TestDataFactory.newsUrl))
+            whenever(newsBackendApi.getNews(any()))
                     .thenReturn(Single.just(TestDataFactory.makeResponseList(5)))
             systemUnderTest = NewsService(newsBackendApi)
         }
+    
+    @Test
+    fun checkIfEndpointHasCorrectBaseUrl (){
+        val urlCaptor = argumentCaptor<String>()
+        whenever(newsBackendApi.getNews(urlCaptor.capture()))
+                .thenReturn(Single.just(TestDataFactory.makeResponseList(5)))
+        val newsObserver = systemUnderTest.getNews().test()
+        assertEquals("http://omnisport-article.performfeeds.com/",urlCaptor.firstValue)
+    }
 
    @Test
     fun loadAllNews (){
@@ -39,7 +41,14 @@ class NewsServiceTest {
        newsObserver.assertNoErrors()
        newsObserver.assertValueCount(1)
        newsObserver.assertComplete()
+    }
 
+    @Test
+    fun loadAllNewsErrorHandling (){
+        whenever(newsBackendApi.getNews(TestDataFactory.newsUrl))
+                .thenReturn(Single.error<Response>(RuntimeException()))
+        val newsObserver = systemUnderTest.getNews().test()
+        newsObserver.assertComplete()
     }
 
 }
